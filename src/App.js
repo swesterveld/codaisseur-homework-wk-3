8b950f4600dd9aa3d691b3a57fe7e0fd55974648
Sil Westerveld <git@silwesterveld.com>
2018-11-02 20:10:36 +0100

    Add data to use and a comment containing the expected output

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const data = {
+  "Ivel Z3": {
+    manufacturer: "Ivasim",
+    year: 1969,
+    origin: "Croatia"
+  },
+  "Bally Astrocade": {
+    manufacturer: "Bally Consumer Products",
+    year: 1977,
+    origin: "USA"
+  },
+  "Sord M200 Smart Home Computer": {
+    manufacturer: "Sord Computer Corporation",
+    year: 1971,
+    origin: "Japan"
+  },
+  "Commodore 64": {
+    manufacturer: "Commodore",
+    year: 1982,
+    origin: "USA"
+  }
+}
+
 class App extends Component {
+  /*
+  <!-- Expected Output -->
+  <select>
+    <option value="">-- pick a model --</option>
+    <option value="Ivel Z3">Ivel Z3 (1969)</option>
+    <option value="Bally Astrocade">Bally Astrocade (1977)</option>
+    <option value="Sord M200 Smart Home Computer">Sord M200 Smart Home Computer (1971)</option>
+    <option value="Commodore 64">Commodore 64 (1982)</option>
+  </select>
+  */
   render() {
     return (
       <div className="App">
