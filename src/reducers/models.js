0f0505753673b507a98380beaa9300427fe76b5e
Sil Westerveld <git@silwesterveld.com>
2018-11-03 17:19:36 +0100

    Remove obsolete console log

--- src/reducers/models.js
+++ src/reducers/models.js
@@ -3,7 +3,6 @@ import {ADD_MODEL} from "../actions/models";
 export default (state = [], action = {}) => {
   switch (action.type) {
     case ADD_MODEL:
-      console.log("TODO: Implement action ADD_MODEL")
       if (action.payload)
         return [...state, action.payload]
       return state
